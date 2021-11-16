5cf5c0bdec1247e7769ec4917a261a9f1fd5e1e1
Lizzie Anthony <lizziewrab@me.com>
2021-11-16 12:16:35 -0600

    commit tree 7268f0a7b63f7dc4a0523057f93fb003c4391cfa

--- main.js
+++ main.js
@@ -10,7 +10,7 @@ const FULL_HEART = '♥'
 //------------------------------------------------------------------------------
 // Don't change the code below: this function mocks the server response
 //------------------------------------------------------------------------------
-
+//replacing the fetch 
 function mimicServerCall(url="http://mimicServer.example.com", config={}) {
   return new Promise(function(resolve, reject) {
     setTimeout(function() {
@@ -23,3 +23,33 @@ function mimicServerCall(url="http://mimicServer.example.com", config={}) {
     }, 300);
   });
 }
+
+const hearts = document.querySelectorAll(".like-glyph")
+for (heart of hearts){
+  heart.addEventListener("click", likeCallback)
+  console.log(heart);
+}
+
+function likeCallback(e){
+
+let heart = e.target
+mimicServerCall()
+.then (() => {
+  if(heart.target.innerText === EMPTY_HEART) {
+    heart.target.innerText = FULL_HEART
+  }
+  else if (hearts.target.innerText=== FULL_HEART) {
+    heart.target.innerText = EMPTY_HEART
+  }
+})
+.catch(() => {
+  const erMsg = document.getElementById("modal")
+  console.log(erMsg)
+  erMsg.className = "hidden"
+
+  setTimeout(() => {
+    const erMsg = document.getElementById("modal")
+    console.log(erMsg)
+    erMsg.className = "hidden"}, 3000)}
+    )}
+ 
